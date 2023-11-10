2243d5871daedf180f2dda34cc37efa4fd4f2d46
Jesalt1 <83181906+Jesalt1@users.noreply.github.com>
2023-11-10 13:29:45 -0500

    Add files via upload

---
+++ Aplicaciondeprueba/app/src/main/java/com/example/aplicaciondeprueba/GroupCapture.java
@@ -0,0 +1,22 @@
+package com.example.aplicaciondeprueba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class GroupCapture extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_capture);
+    }
+
+        public void returnMain(View v)
+        {
+            Intent register_view = new Intent(this, MainActivity.class);
+            startActivity(register_view);
+        }
+}
